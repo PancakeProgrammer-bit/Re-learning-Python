ec125ed44643a08d641040c7807d3d3620983409
Saul Codeman <71710299+PancakeProgrammer-bit@users.noreply.github.com>
2023-07-10 21:57:28 -0300

    a
    oh my god, new old things

---
+++ aula_6/aula_6.py
@@ -0,0 +1,16 @@
+print ('Hello world!')
+
+#Aula 6 - Tipos primitivos e inserção de dados 
+
+#Int
+num1 = int(input('Insira 1 número: '))
+num2 = int(input('Insira outro número: '))
+soma = num1 + num2 
+print('A soma entre {} e {} é {}. '.format(num1,num2,soma))
+
+#Float
+
+#Boot
+
+
+#str
