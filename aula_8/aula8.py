c868fcaf93110ee93501f1886cc7b7b27cb9c8c2
Saul Codeman <71710299+PancakeProgrammer-bit@users.noreply.github.com>
2023-08-02 17:07:26 -0300

    a
    a

---
+++ aula_8/aula8.py
@@ -0,0 +1,24 @@
+# Trabalhando com módulos
+# Utilizando o exemplo de inserção de alimentos no corpo humano, utilizaremos:
+# bebida = 'Suco', 'Refri', 'Café', 'Leite'
+# comida = 'Hamburger', 'Pizza', 'Taco', 'Panqueca', 'Lasanha'
+# doce = 'Bolo', 'Torta', 'Sorvete', 'Donut'
+# import bebida
+# import doce
+
+# Basicamente, ao inserir apenas import "nome_da_biblioteca", você puxará todas as informações da biblioteca pra utilizar
+
+# Mas e se eu quiser realizar uma importação única?
+# from doce import bolo #Aqui, estou importando apenas um doce.
+
+# Utilizando a biblioteca math
+import math  # Biblioteca que trás funcionalidades matemáticas extras
+# Por padrão, o Python já vem com os operadores +, -, * e /
+
+# ceil - Faz um arredondamento para cima
+# floor - arredonda falores para baixo
+# trunc - ele vai eliminar informações da vírgula pra frente, ou seja, vai truncar o aplicativo
+# pow - power/potência - funciona da mesma forma dos dois asteríscos
+# sqrt - raíz quadrada
+
+from math import ceil
