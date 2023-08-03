72ab766bd99c2dccab84ba949ce019f450fbdf06
Saul Codeman <71710299+PancakeProgrammer-bit@users.noreply.github.com>
2023-08-02 22:08:57 -0300

    a
    a

---
+++ unopar/aula_4.py
@@ -0,0 +1,21 @@
+# Primeiros passos em Python:
+print('Olá mundo!')
+
+# Estrutura condicional simples:
+nome = 'Rayan'
+sobrenome = ' '
+lista = []
+
+if nome:
+    # Isso só será executado se a condição imposta for verdadeira
+    print("A variável nome não é vazia")
+
+
+# Estrutura composta
+
+valor1 = input('Insira um valor: ')
+valor2 = input('Insira outro valor: ')
+if valor1 > valor2:
+    print("O primeiro valor é maior que o segundo.")
+else:
+    print('O segundo valor é maior que o primeiro.')
--- unopar/test.py
+++ unopar/test.py
@@ -1,2 +1,8 @@
-linguagens = ("Python", "Java" ,"C")
-print ()
+# Estrutura composta
+
+valor1 = input('Insira um valor: ')
+valor2 = input('Insira outro valor: ')
+if valor1 > valor2:
+    print("O primeiro valor é maior que o segundo.")
+else:
+    print('O segundo valor é maior que o primeiro.')
